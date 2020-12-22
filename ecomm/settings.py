8596bf863b80ccd68acde8339e176c9a680020e8
MACmidiDEV <macmidi.webdesigns@gmail.com>
2020-12-22 12:10:20 +0000

    added images styled home page

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.facebook',
-    'allauth.socialaccount.providers.instagram',
     'home',
 ]
 
@@ -147,4 +145,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
