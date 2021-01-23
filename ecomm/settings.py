65560b25feb3c9ad3367f9a305129b60167d47ab
MACmidiDEV <macmidi.webdesigns@gmail.com>
2021-01-23 16:57:32 +0000

    edit

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -22,8 +22,8 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['MACmidiDEV-ecomm-ci-fsd.herokuapp.com', 'localhost']
+                             
+ALLOWED_HOSTS = ['ecomm-ci-fsd.herokuapp.com','MACmidiDEV-ecomm-ci-fsd.herokuapp.com', 'localhost']
 
 
 # Application definition
